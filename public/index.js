650ca6c606bbb78184092dbf5c4008caa5a1e3e6
Chandanagouda <chandanhiregoudra@gmail.com>
2026-03-08 16:28:35 +0530

    Initial commit - calculator project

---
+++ public/index.js
@@ -0,0 +1,59 @@
+const express = require('express');
+const app = express();
+
+app.use(express.json());
+
+app.get('/',function(req,res){
+    res.sendFile("C:/cohort4.0/Projects/calculator/public/index.html");
+})
+
+//using query parameters
+// https://localhost/sum?a=1&&b=2
+app.get('/sum',function(req,res){
+    const a = parseInt(req.query.a);
+    const b = parseInt(req.query.b);
+    
+    const sum = a+b;
+
+    res.json({
+        ans : sum
+    });
+})
+
+//using path parametes
+// https://localhost/sub/5/2
+app.get('/sub/:a/:b',function(req,res){
+    const a = parseInt(req.params.a);
+    const b = parseInt(req.params.b);
+
+    res.json({
+        ans: a-b
+    });
+});
+
+app.post('/mul',function(req,res){
+     const a = parseInt(req.body.a);
+     const b = parseInt(req.body.b);
+
+     const prod = a*b;
+     res.json({
+        ans: prod
+     });
+});
+
+app.post('/div',function(req,res){
+    const a = parseInt(req.body.a);
+    const b = parseInt(req.body.b);
+
+    if(b === 0){
+        return res.status(400).json({
+            error:"Division by zero not allowed"
+        });
+    }
+
+    res.json({
+        ans: a/b
+    })
+})
+
+app.listen(3002);
